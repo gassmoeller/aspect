904de8f06c19edc97050519c79412023fed96113
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2018-04-23 16:22:36 -0700

    Trick tester to ignore manual changes to tests/CMakeLists.txt

--- cmake/generate_reference_output.sh
+++ cmake/generate_reference_output.sh
@@ -14,7 +14,7 @@ SRC_PATH=`cd $SRC_PATH/..;pwd`
 OUT=$PWD/changes.diff
 
 if [ "$ASPECT_TESTS_VERBOSE" == "1" ]; then
-  ASPECT_GENERATE_REFERENCE_OUTPUT=1 ctest --output-on-failure -j 4 -V
+  ASPECT_GENERATE_REFERENCE_OUTPUT=1 ctest --output-on-failure -j 4
 else
   ASPECT_GENERATE_REFERENCE_OUTPUT=1 ctest --output-on-failure -j 4 >/dev/null
 fi
