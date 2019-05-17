938e79df90be813312a4649259e27c43d68c8634
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2019-05-17 14:32:10 -0700

    Update time-dependent and add results

--- benchmarks/rigid_shear/time-dependent/creates_formatted_tables.sh
+++ benchmarks/rigid_shear/time-dependent/creates_formatted_tables.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 grid_resolution=('3' '4' '5' '6')
-PPD=('4' '5' '6' '7' '10' '15' '20')
 interpolators=('cell_average' 'bilinear_least_squares')
 integrators=('rk2' 'rk4')
 stokes_degrees=('2' '3')
@@ -9,8 +8,14 @@ stokes_degrees=('2' '3')
 for stokes_degree in ${stokes_degrees[@]}; do
   for interpolator in ${interpolators[@]}; do
   for integrator in ${integrators[@]}; do
-  outputfile=results_Q${stokes_degree}_${interpolator}_${integrator}.dat
-  rm -f $outputfile
+    outputfile=results_Q${stokes_degree}_${interpolator}_${integrator}.dat
+    rm -f $outputfile
+
+    if [ $stokes_degree == 2 ]; then
+      PPD=('4' '5' '6' '7' '10' '15' '20')
+    else
+      PPD=('4' '5' '6' '7' '10' '15' '20' '32' '45' '64' '80')
+    fi
 
     for ppd in ${PPD[@]}; do
       for grid_res in ${grid_resolution[@]}; do
@@ -18,7 +23,7 @@ for stokes_degree in ${stokes_degrees[@]}; do
         ppc=`expr $ppd \* $ppd`
         echo -n $h $ppc >> $outputfile
 
-        current_dir=Q2_Pfalse_refinement${grid_res}_${ppd}_reference_cell_${interpolator}_${integrator}
+        current_dir=Q${stokes_degree}_Pfalse_refinement${grid_res}_${ppd}_reference_cell_${interpolator}_${integrator}
         echo $current_dir
 
         if [ -f ${current_dir}/log.txt ]; then
