d81914ddc2824f271dc10d21efb25fd382bcdf06
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2019-08-21 11:49:43 -0700

    temp

--- contrib/package/build.sh
+++ contrib/package/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#apt-get update && apt-get upgrade && apt-get install -yq --no-install-recommends build-essential cmake gcc g++ gfortran git libblas-dev liblapack-dev libmpich-dev mpich numdiff zlib1g-dev fuse wget ca-certificates lsb-release
+apt-get update && apt-get upgrade -yq && apt-get install -yq --no-install-recommends build-essential cmake gcc g++ gfortran git libblas-dev liblapack-dev libopenmpi-dev openmpi-bin numdiff zlib1g-dev fuse wget ca-certificates lsb-release file # libmpich-dev mpich
 
-#git clone https://github.com/dealii/candi.git/
-#cd candi
-#./candi.sh -j6 -p ${PWD}/../AppImage --packages="hdf5 p4est trilinos dealii"
-#cd ../AppImage
+git clone https://github.com/dealii/candi.git/
+cd candi
+./candi.sh -j6 -p ${PWD}/../AppImage --packages="hdf5 p4est trilinos dealii"
+cd ../AppImage
 git clone https://github.com/geodynamics/aspect.git/
 mkdir build && cd build
-cmake -D ASPECT_PRECOMPILE_HEADERS=ON ../aspect
+cmake -D ASPECT_PRECOMPILE_HEADERS=ON -DDEAL_II_DIR=../deal.II-v9.1.1 ../aspect
 make release
 make -j6
 
@@ -18,6 +18,6 @@ chmod +x linuxdeploy-x86_64.AppImage
 ./linuxdeploy-x86_64.AppImage --appimage-extract
 
 export VERSION=dev
-squashfs-root/AppRun -e ${PWD}/build/aspect -d ${PWD}/aspect/contrib/package/aspect.desktop -i ${PWD}/aspect/contrib/package/mesh-2d-icon.png --appdir AppDir
+squashfs-root/AppRun -e ${PWD}/build/aspect -d ${PWD}/aspect/contrib/package/aspect.desktop -i ${PWD}/aspect/contrib/package/mesh-2d-icon.png --appdir AppDir --output appimage
 squashfs-root/AppRun --appdir AppDir --output appimage
 cp ASPECT-x86_64.AppImage software/
