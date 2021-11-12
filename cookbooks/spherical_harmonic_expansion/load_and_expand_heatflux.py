ee6cb64648cefb73d437d69bfba5e87d64b86b37
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2021-11-12 09:58:01 -0500

    Expand into SPH and write netcdf

--- cookbooks/spherical_harmonic_expansion/load_and_expand_heatflux.py
+++ cookbooks/spherical_harmonic_expansion/load_and_expand_heatflux.py
@@ -9,41 +9,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 from mpl_toolkits import mplot3d
-
-import vtk as vtk; from vtk.util import numpy_support
 import scipy.interpolate as interpolate
-
-
-def load_vtk(filename):
-  # Load vtu data (pvtu directs to vtu files)
-  reader = vtk.vtkXMLPUnstructuredGridReader()
-  reader.SetFileName(filename)
-  reader.Update()
-
-  # Get the coordinates of nodes in the mesh
-  nodes_vtk_array= reader.GetOutput().GetPoints().GetData()
-
-  # Convert nodal vtk data to a numpy array
-  nodes_numpy_array = vtk.util.numpy_support.vtk_to_numpy(nodes_vtk_array)
-
-  # Extract x, y and z coordinates from numpy array 
-  x,y = nodes_numpy_array[:,0] , nodes_numpy_array[:,1]
-
-  # Determine the number of scalar fields contained in the .pvtu file
-  number_of_fields = reader.GetOutput().GetPointData().GetNumberOfArrays()
-
-  # Determine the name of each field and place it in an array.
-  field_names = []
-  for i in range(number_of_fields):
-    field_names.append(reader.GetOutput().GetPointData().GetArrayName(i))
-
-  # Determine the index of the field strain_rate
-  idx = field_names.index("heat_flux_map")
-
-  # Extract values of strain_rate
-  field_vtk_array = reader.GetOutput().GetPointData().GetArray(idx)
-  strain_rate     = numpy_support.vtk_to_numpy(field_vtk_array)
-  return x, y, strain_rate
+import pyshtools as pysh
 
 # Relative (from current directory) path to the
 # solution data folder.
@@ -51,32 +18,30 @@ mypath = os.path.dirname(__file__)
 output_directory = os.path.join(mypath, 'output-spherical_harmonic_expansion')
 solution_directory = os.path.join(output_directory, 'solution')
 
-# Time step to analyze. This number corresponds to 
-# the numbering of .pvtu files. In this instance
-# '00000.0213' is the last nonlinear iteration
-# of the first time step, but note that the 
-# number of nonlinear iterations may vary slightly
-# between machines, or significantly when additional
-# parameters are altered.
-number = '00000'
+# Time step to analyze.
+output_number = '00000'
 
-vtu_filename = os.path.join(solution_directory, 'solution-' + number + '.pvtu')
-text_filename = os.path.join(output_directory, 'heat_flux.' + number)
+text_filename = os.path.join(output_directory, 'heat_flux.' + output_number)
 
 #heat_flux = np.genfromtxt(text_filename, delimiter=' ', skip_header=1)
 heat_flux = pd.read_csv(text_filename, delimiter=' ', skiprows=1, header=0, names=['x','y','z','heat_flux'])
 
 heat_flux['r'] = heat_flux.apply(lambda row: np.sqrt(row.x*row.x+row.y*row.y+row.z*row.z), axis = 1)
 heat_flux['latitude'] = heat_flux.apply(lambda row: 90 - np.arccos(row.z/row.r) * 180 / np.pi, axis = 1)
-heat_flux['longitude'] = heat_flux.apply(lambda row: np.arctan2(row.y,row.x) * 180 / np.pi, axis = 1)
+heat_flux['longitude'] = heat_flux.apply(lambda row: 180 + np.arctan2(row.y,row.x) * 180 / np.pi, axis = 1)
 
 bottom_heat_flux = heat_flux[heat_flux.r < 4e6].to_numpy()
 
+# Gauss Legendre sample points
+lmax = 32
+latglq, longlq = pysh.shtools.GLQGridCoord (lmax)
 
-lat_i = np.linspace(-90, 90, 51)
-long_i = np.linspace(-180, 180, 51)
+# Equally spaced sample points
+#lat_i = np.linspace(-90, 90, 51)
+#long_i = np.linspace(-180, 180, 51)
+
 # Generate data interpolation grid. 
-Lat, Lon = np.meshgrid(lat_i,long_i)
+Lon, Lat = np.meshgrid(longlq,latglq)
 
 # create interpolation function
 #function = interpolate.interp2d(bottom_heat_flux[:,5], bottom_heat_flux[:,6], bottom_heat_flux[:,3], kind='linear')
@@ -84,39 +49,62 @@ Lat, Lon = np.meshgrid(lat_i,long_i)
 #bottom_heat_flux_interpolated = function(lat_i, long_i)
 
 # Plot heat flux field
-fig = plt.figure()
+fig, ax = plt.subplots(2,2)
 
 # Cartesian scatter
 # ax0 = fig.gca(projection='3d')
 # c = ax0.scatter(bottom_heat_flux[:,0],bottom_heat_flux[:,1],bottom_heat_flux[:,2],c=bottom_heat_flux[:,3])
 
 # Geographic scatter
-ax0 = fig.add_subplot(211)
-c = ax0.scatter(bottom_heat_flux[:,6],bottom_heat_flux[:,5],c=bottom_heat_flux[:,3])
+c = ax[0,0].scatter(bottom_heat_flux[:,6],bottom_heat_flux[:,5],c=bottom_heat_flux[:,3],s=1)
+ax[0,0].set_aspect('equal', 'box')
+ax[0,0].set_xlabel('Longitude')
+ax[0,0].set_ylabel('Latitude')
+ax[0,0].set_title('Heat flux on cell centers', fontsize=10)
+fig.colorbar(c, ax=ax[0,0])
 
 #ax1 = fig.add_subplot(212)
 #d = ax1.scatter(Lon, Lat,c=bottom_heat_flux_interpolated)
 
 # Geographic contour
 # Interpolate heat flux onto new grid
-heat_flux_structured = interpolate.griddata((bottom_heat_flux[:,5:]), bottom_heat_flux[:,3], (Lat,Lon), method='cubic')
-ax1 = fig.add_subplot(212)
-d = ax1.pcolormesh(Lon,Lat,heat_flux_structured)
+heat_flux_structured = interpolate.griddata((bottom_heat_flux[:,5:]), bottom_heat_flux[:,3], (Lat, Lon), method='cubic', fill_value=0.0)
+d = ax[1,0].pcolormesh(Lon,Lat,heat_flux_structured, shading='auto')
+ax[1,0].set_aspect('equal', 'box')
+ax[1,0].set_xlabel('Longitude')
+ax[1,0].set_ylabel('Latitude')
+ax[1,0].set_title('Heat flux on GLQ points', fontsize=10)
+fig.colorbar(d, ax=ax[1,0])
 
 
-ax0.set_aspect('equal', 'box')
-ax0.set_xlabel('Longitude')
-ax0.set_ylabel('Latitude')
-ax0.set_title('Heat flux on cell centers', fontsize=10)
+# Spherical harmonic expansion
+zero, w = pysh.expand.SHGLQ (lmax)
+cilm = pysh.expand.SHExpandGLQ (heat_flux_structured, w, zero, norm=2, csphase=-1)
+power_per_l = pysh.spectralanalysis.spectrum(cilm,normalization='schmidt')
+#power_per_l = np.arange(cilm.shape[1])
+degrees = np.arange(cilm.shape[1])
+e = ax[0,1].plot(degrees, power_per_l)
+ax[0,1].set_xlim(0, lmax)
+ax[0,1].set_ylim(3e-7, 3e-3)
+ax[0,1].set_yscale('log')
+ax[0,1].set_xlabel('SPH degree')
+ax[0,1].set_ylabel('Power')
+ax[0,1].set_title('Heat flux spectrum', fontsize=10)
 
-ax1.set_aspect('equal', 'box')
-ax1.set_xlabel('Longitude')
-ax1.set_ylabel('Latitude')
-ax1.set_title('Heat flux on GLQ points', fontsize=10)
+# Expand spherical harmonic expansion onto new grid
+heat_flux_grid = pysh.expand.MakeGridGLQ(cilm, zero, lmax, norm=2, csphase=-1)
+d = ax[1,1].pcolormesh(Lon,Lat,heat_flux_grid, shading='auto')
+ax[1,1].set_aspect('equal', 'box')
+ax[1,1].set_xlabel('Longitude')
+ax[1,1].set_ylabel('Latitude')
+ax[1,1].set_title('Heat flux from SPH on GLQ points', fontsize=10)
+fig.colorbar(d, ax=ax[1,1])
 
-fig.colorbar(c, ax=ax0)
-fig.colorbar(d, ax=ax1)
-
-plt.show()
+# Output spherical harmonic expansion
+coeffs = pysh.SHCoeffs.from_array(cilm, normalization='schmidt', csphase=-1, lmax=lmax, name='heatflux')
+coeffs.info()
+coeffs.to_file(os.path.join(mypath, 'heat_flux_coeffs.txt'))
+coeffs.to_netcdf(os.path.join(mypath, 'heat_flux_coeffs.nc'))
+#plt.show()
 plt.savefig("heat_flux.png", dpi=300)
 plt.close()
