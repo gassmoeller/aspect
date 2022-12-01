766e30260d9af36049dbfff873bab42144bae5bc
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2022-12-01 12:02:00 -0500

    Rename run script

---
+++ benchmarks/annulus/run_all_models.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Instructions for how to use this scipt are provided in the README.
+
+processes=8
+ASPECT_EXEC="./plugin/aspect"
+
+for refinement in 6 7; do #0 1 2 3 4 5
+  for k in 0 1 2 4 8; do
+    echo "subsection Mesh refinement" > current.prm
+    echo "  set Initial global refinement = $refinement" >> current.prm
+    echo "end" >> current.prm
+
+    echo "subsection Annulus benchmark" >> current.prm
+    echo "  set k = $k" >> current.prm
+    echo "end" >> current.prm
+
+    echo "set Output directory = output-refinement_${refinement}_k_${k}" >> current.prm
+
+    cat annulus.prm current.prm | mpirun -np $processes $ASPECT_EXEC --
+  done
+done
