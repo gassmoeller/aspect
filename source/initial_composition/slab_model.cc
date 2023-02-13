456f6197a6a2d86c54c7ee0f20366c0fce687133
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2023-02-13 08:53:39 -0500

    Address comments

--- source/initial_composition/slab_model.cc
+++ source/initial_composition/slab_model.cc
@@ -34,7 +34,7 @@ namespace aspect
     SlabModel<dim>::initialize ()
     {
       AssertThrow(this->introspection().compositional_name_exists("slabs"),
-                  ExcMessage("The initial composition plugin `slabs' did not find a "
+                  ExcMessage("The initial composition plugin `slab model' did not find a "
                              "compositional field called `slabs' to initialize. Please add a "
                              "compositional field with this name."));
 
@@ -57,15 +57,15 @@ namespace aspect
       if (compositional_index != slab_index)
         return 0.0;
 
-      // The first data column correspond to the slab depth and the second to slab thickness
+      // The first data column corresponds to the slab depth and the second column to the slab thickness.
       // 'Slab depth' stands for the depth of the upper surface of the slab, 'Slab thickness'
-      // for the vertical distance between upper and lower surface. In lateral positions that
-      // do not have a slab thickness should be set to 0.0 and depth to a very large value
-      // that is outside the model domain.
+      // for the vertical distance between upper and lower surface.
       const double slab_depth     = slab_boundary.get_data_component(surface_boundary_id, position, 0);
       const double slab_thickness = slab_boundary.get_data_component(surface_boundary_id, position, 1);
 
-      // Return 0.0 if there is no slab in this location in the data
+      // Return 0.0 if there is no slab in this location in the data. No slab is encoded in
+      // the data file as a slab thickness of 0.0 and/or a depth larger than the depth of
+      // the domain.
       if (slab_thickness == 0.0 ||
           slab_depth > this->get_geometry_model().maximal_depth())
         return 0.0;
@@ -132,7 +132,7 @@ namespace aspect
                                               "into an aspect input data file is available in the directory "
                                               "data/initial-composition/slab-model/. Please note that "
                                               "Slab2 and the example data file assume spherical geometry "
-                                              "(latitude, longitude coordinates), however that is not "
+                                              "(latitude, longitude coordinates), however, that is not "
                                               "necessary for this plugin, data files in "
                                               "cartesian coordinates will work with box geometries.")
   }
