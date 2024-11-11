841fd5573f6c9459853b3169ca3170271a1ce96c
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2024-11-11 17:03:38 +0100

    tmp

--- benchmarks/newton_solver_benchmark_set/tosi_et_al_2015/bash.sh
+++ benchmarks/newton_solver_benchmark_set/tosi_et_al_2015/bash.sh
@@ -1,14 +1,14 @@
 !/bin/bash
-version="test"
+version="a9"
 build_dir="../../../build/"
 declare -a grid=(5) 
 declare -a UFS=("false" "true") # use failsafe
-declare -a NSP=("SPD") # "Symmetric" "PD" "none") #Use Newton stabilization preconditioner
-declare -a NSA=("SPD" "symmetric" "none") # "Symmetric" "PD" "none") #Use Newton stabilization A block
+declare -a NSP=("SPD") # "Symmetric" "PD" "none") # Use Newton stabilization preconditioner
+declare -a NSA=("SPD") # "symmetric"  "PD" "none" # Use Newton stabilization A block
 declare -a agrid=(0)
 I=150
-declare -a P=(5 150 0 10 50 15 20 25 30) #0 5 10 15 20 150) # 1 2 3 4 5 10 15 25 50 100)
-declare -a LS=(50)
+declare -a P=(5 150) #0 5 10 15 20 25 150) # 1 2 3 4 5 10 15 25 50 100)
+declare -a LS=(0) # 50 # No line search iterations as reported in the paper.
 declare -a ST=(1e-20)
 declare -a LT=("1e-5") # "1e-8") # "1e-8") #"1e-5") # "1e-8")
 declare -a NLT=("1e-5") # "1e-9" "1e-10" "1e-14" "1e-20")
@@ -19,10 +19,10 @@ declare -a UDS=("false")
 declare -a AEW=("false") # always use Eisenstat Walker, even for picard
 AV=-1
 COMP="0" #"4e-12"
-declare -a OS=("9e-1" "1e-1" "1e-2" "1e-8") # "1e-3" "1e-4" "1e-5" "1e-6" "1e-6" "1e-7" "1e-8")
+declare -a MLT=("9e-1" "1e-2") # "1e-3" "1e-4" "1e-5" "1e-6" "1e-6" "1e-7" "1e-8")
 materialmodelnameShort="TS" #"VP2"
 materialmodelname="tosi"
-processes=2
+processes=6
 
 if [ $materialmodelnameShort == "TS" ]; then
  materialmodelname="tosi"
@@ -75,12 +75,12 @@ do
             do
              for i_AEW in "${AEW[@]}"
              do
-              for i_OS in "${OS[@]}"
+              for i_MLT in "${MLT[@]}"
               do
                for i_RSM in "${RSM[@]}"
                do
                
-               dirname_clean="$version""$materialmodelnameShort""_""$SOLVER_SHORT""_ST""$i_ST""_UFS""$i_UFS""_NSP-""$i_NSP""_NSA-""$i_NSA""_C""$COMP""_g""$i_grid""_ag""$i_agrid""_AEW""$i_AEW""_UDS""$i_UDS""_SF""$i_SF""_NLT""$i_NLT""_ABT""$i_ABT""_LT""$i_LT""_mLT""$i_OS""_I""$I""_P""$i_P""_EW1""_theta1""_LS""$i_LS""_RSM""$i_RSM""_AV""$AV"
+               dirname_clean="$version""$materialmodelnameShort""_""$SOLVER_SHORT""_ST""$i_ST""_UFS""$i_UFS""_NSP-""$i_NSP""_NSA-""$i_NSA""_C""$COMP""_g""$i_grid""_ag""$i_agrid""_AEW""$i_AEW""_UDS""$i_UDS""_SF""$i_SF""_NLT""$i_NLT""_ABT""$i_ABT""_LT""$i_LT""_mLT""$i_MLT""_I""$I""_P""$i_P""_EW1""_theta1""_LS""$i_LS""_RSM""$i_RSM""_AV""$AV"
                dirname="results/""$dirname_clean"
                infilename="$dirname""/input.prm"
                outfilename="$dirname""/output.log"
@@ -109,7 +109,7 @@ do
                 -e "s/set SPD safety factor.*/set SPD safety factor = $i_SF/g" \
                 -e "s/set Use deviator of strain-rate.*/set Use deviator of strain-rate = $i_UDS/g" \
                 -e "s/set Max Newton line search iterations.*/    set Max Newton line search iterations = $i_LS/g" \
-                -e "s/set Maximum linear Stokes solver tolerance.*/set Maximum linear Stokes solver tolerance = $i_OS/g" \
+                -e "s/set Maximum linear Stokes solver tolerance.*/set Maximum linear Stokes solver tolerance = $i_MLT/g" \
                 -e "s/set Use Newton residual scaling method.*/    set Use Newton residual scaling method = $i_RSM/g" \
                 input.prm > "$infilename"
                
