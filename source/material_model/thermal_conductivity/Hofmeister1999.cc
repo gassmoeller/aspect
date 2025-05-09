a86f5192a54a571386e31607b468e390a4058f51
Enrico Marzotto <enricomarzotto@yahoo.it>
2025-05-09 14:23:58 +0200

    Added function to compute Rad TCond Hofmeister1999

--- source/material_model/thermal_conductivity/Hofmeister1999.cc
+++ source/material_model/thermal_conductivity/Hofmeister1999.cc
@@ -36,15 +36,23 @@ namespace aspect
   {
     namespace ThermalConductivity
     {
-      // Helper function: Compute lattice thermal conductivity
-      double compute_lattice_thermal_conductivity(double Lambda0, double N_Texp, double Gamma, double Alpha, double K0, double K_prime, double T_room, double T_model, double P_model)
+      // Helper function: Compute the lattice thermal conductivity using the Hofmeister (1999) formulation
+      double Compute_Lat_TCond_Hofmeister1999(double Lambda0, double N_Texp, double Gamma, double Alpha, double K0, double K_prime, double T_room, double T_model, double P_model)
       {
-        // Compute the lattice thermal conductivity in real (+,-) and simplex (0->1) space considering the boundaries (ymin) and (ymax)
+        // Lambda_Lat(P,T) [W m^-1 K^-1] = Lambda_Room(T_room/T_model)^N_Texp * exp[-(4*Gamma + 1/3)*Alpha*(T_model-T_room)] * (1+(K_prime*P_model/K0))
         double Factor_1 = Lambda0 * std::pow((T_room / T_model), N_Texp);
-        double Factor_2 = exp(-((4.*Gamma)+(1/3))*(Alpha*(T_model-298)));
+        double Factor_2 = exp(-((4*Gamma)+(1/3))*(Alpha*(T_model-298)));
         double Factor_3 = (1+((K_prime*P_model)/K0));
         double HLatTCon = Factor_1 * Factor_2 * Factor_3;
         return HLatTCon;
+      }
+
+      // Helper function: Compute the radiative thermal conductivity using the Hofmeister (1999) formulation
+      double Compute_Rad_TCond_Hofmeister1999(double A0, double B1, double C2, double D3, double T_model)
+      {
+        // Lambda_Rad(T)   [W m^-1 K^-1] = A0 - B1*T + C2*T^2 + D3*T^3
+        double HRadTCon = A0 + (B1 * T_model) + (C2 * std::pow(T_model, 2)) + (D3 * std::pow(T_model, 3));
+        return HRadTCon;
       }
 
       // Main function: 
@@ -104,15 +112,22 @@ namespace aspect
         // Take the temperature field of the model [K]
         double T_mod = in.temperature[1];
 
-        double OlivineDry_Hof99_LatTCon = compute_lattice_thermal_conductivity(
+        double OlivineDry_Hof99_LatTCon = Compute_Lat_TCond_Hofmeister1999(
         OlivineDry_Hof99_LatTC_Room, OlivineDry_Hof99_LatTC_TExp, OlivineDry_Hof99_LatTC_GruP, OlivineDry_Hof99_LatTC_Alph,
         OlivineDry_Hof99_LatTC_Bulk, OlivineDry_Hof99_LatTC_PDev, T_room, T_mod, P_GPa); 
-        double WadsleyDry_Hof99_LatTCon = compute_lattice_thermal_conductivity(
+        double WadsleyDry_Hof99_LatTCon = Compute_Lat_TCond_Hofmeister1999(
         WadsleyDry_Hof99_LatTC_Room, WadsleyDry_Hof99_LatTC_TExp, WadsleyDry_Hof99_LatTC_GruP, WadsleyDry_Hof99_LatTC_Alph,
         WadsleyDry_Hof99_LatTC_Bulk, WadsleyDry_Hof99_LatTC_PDev, T_room, T_mod, P_GPa); 
-        double RingwooDry_Hof99_LatTCon = compute_lattice_thermal_conductivity(
+        double RingwooDry_Hof99_LatTCon = Compute_Lat_TCond_Hofmeister1999(
         RingwooDry_Hof99_LatTC_Room, RingwooDry_Hof99_LatTC_TExp, RingwooDry_Hof99_LatTC_GruP, RingwooDry_Hof99_LatTC_Alph,
         RingwooDry_Hof99_LatTC_Bulk, RingwooDry_Hof99_LatTC_PDev, T_room, T_mod, P_GPa); 
+
+        double OlivineDry_Hof99_RadTCon = Compute_Rad_TCond_Hofmeister1999(
+        OlivineDry_Hof99_RadTC_CoA0, OlivineDry_Hof99_RadTC_CoB1, OlivineDry_Hof99_RadTC_CoC2, OlivineDry_Hof99_RadTC_CoD3, T_mod); 
+        double WadsleyDry_Hof99_RadTCon = Compute_Rad_TCond_Hofmeister1999(
+        WadsleyDry_Hof99_RadTC_CoA0, WadsleyDry_Hof99_RadTC_CoB1, WadsleyDry_Hof99_RadTC_CoC2, WadsleyDry_Hof99_RadTC_CoD3, T_mod); 
+        double RingwooDry_Hof99_RadTCon = Compute_Rad_TCond_Hofmeister1999(
+        RingwooDry_Hof99_RadTC_CoA0, RingwooDry_Hof99_RadTC_CoB1, RingwooDry_Hof99_RadTC_CoC2, RingwooDry_Hof99_RadTC_CoD3, T_mod); 
 
         std::vector<double> Hofmeister99_Tcond(5, OlivineDry_Hof99_LatTCon);
         out.thermal_conductivities = Hofmeister99_Tcond;
