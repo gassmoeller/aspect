e857a8bfedf4551cb3a58fdd414a107a12b1301c
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2025-07-02 11:32:40 +0200

    Rename no averaging case

---
+++ benchmarks/nsinker/run_solver_comparison.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+for averaging in none "harmonic average"; do # arithmetic/geometric/harmonic average
+  for nsinkers in 4 16; do
+    for viscosity in 1e6; do
+      for refinement in 4; do # 3
+        for solver in "nsinker" "nsinker_gmg" "nsinker_direct"; do
+          echo "subsection Material model" > current.prm
+          echo "  set Material averaging = $averaging" >> current.prm
+          echo "  subsection NSinker" >> current.prm
+          echo "    set Number of sinkers = $nsinkers" >> current.prm
+          echo "    set Dynamic viscosity ratio = $viscosity" >> current.prm
+          echo "  end" >> current.prm
+          echo "end" >> current.prm
+
+          echo "subsection Mesh refinement" >> current.prm
+          echo "  set Initial global refinement = $refinement" >> current.prm
+          echo "  set Initial adaptive refinement = 0" >> current.prm
+          echo "end" >> current.prm
+
+          current_model="${solver}_averaging${averaging// /_}_nsinkers${nsinkers}_viscosity${viscosity}_refinement${refinement}"
+          echo "set Output directory = output-${current_model}" >> current.prm
+          echo "Starting ${current_model}"
+          cat ${solver}.prm current.prm | mpirun -np 8 ./aspect-release --
+        done
+      done
+    done
+  done
+done
