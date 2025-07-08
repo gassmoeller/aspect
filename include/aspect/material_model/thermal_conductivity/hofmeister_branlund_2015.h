eb9a99a87a4a814cc9d567b066ad2fa01daa1d5e
Enrico Marzotto <enricomarzotto@yahoo.it>
2025-07-08 11:07:15 +0200

    Move comments from hofmeister_branlund_2015.cc to header

--- include/aspect/material_model/thermal_conductivity/hofmeister_branlund_2015.h
+++ include/aspect/material_model/thermal_conductivity/hofmeister_branlund_2015.h
@@ -19,6 +19,12 @@
 
 */
 
+// This function computes the thermal conductivity of the oceanic crust (pyroxene, plagioclase)
+// using the Hofmeister & Brandlund (2015) formulation
+// [Hofmeister & Branlund 2015, Treatise on Geophysics, Vol. 2, chap. 2.23, pp. 583–608]
+// https://doi.org/10.1016/B978-0-444-53802-4.00047-6
+// Lambda_Rad (T) = 1.2170 + ( 407.34 / (T_model+0.000080555 * T_model) )
+
 #ifndef _aspect_material_model_thermal_conductivity_hofmeister_branlund_2015_h
 #define _aspect_material_model_thermal_conductivity_hofmeister_branlund_2015_h
 
--- source/material_model/thermal_conductivity/hofmeister_branlund_2015.cc
+++ source/material_model/thermal_conductivity/hofmeister_branlund_2015.cc
@@ -19,10 +19,6 @@
 
 */
 
-// This function computes the thermal conductivity of the oceanic crust (pyroxene, plagioclase)
-// using the Hofmeister & Brandlund (2015) formulation
-// [Hofmeister & Branlund 2015, Treatise on Geophysics, Vol. 2, chap. 2.23, pp. 583–608]
-// https://doi.org/10.1016/B978-0-444-53802-4.00047-6
 // Lambda_Rad (T) = 1.2170 + ( 407.34 / (T_model+0.000080555 * T_model) )
 
 #include <aspect/material_model/thermal_conductivity/hofmeister_branlund_2015.h>
